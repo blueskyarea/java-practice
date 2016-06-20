eba7d22dcd86eb1e2b957765737a75ac4d2ee03b
mh <bluesky@example.com>
2016-06-20 22:01:07 +0900

    How to use StringUtils.join

---
+++ src/practice/stringUtils/Join.java
@@ -0,0 +1,23 @@
+package practice.stringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Join {
+	public static void main(String[] args) {
+		// array
+		String[] array = {"a1", "b1", "c1"};
+		String joined1 = StringUtils.join(array, ",");
+		System.out.println(joined1);	// a1,b1,c1
+		
+		// list
+		List<String> list = new ArrayList<String>();
+		list.add("a2");
+		list.add("b2");
+		list.add("c2");
+		String joined2 = StringUtils.join(list, ",");
+		System.out.println(joined2);	// a2,b2,c2
+	}
+}
