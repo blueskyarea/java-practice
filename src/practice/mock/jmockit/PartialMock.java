779005bf696c61fed2205f62c55339b41c4fc09b
mh <bluesky@example.com>
2017-06-28 23:29:45 +0900

    PartialMock

---
+++ src/practice/mock/jmockit/PartialMock.java
@@ -0,0 +1,15 @@
+package practice.mock.jmockit;
+
+public class PartialMock {
+	public PartialMock(){
+	}
+	
+	public int methodA(int a) {
+		int b = methodB();
+		return a + b;
+	}
+	
+	public int methodB() {
+		return 5;
+	}
+}
