598d33f11c1e662df39e74b9aadf952f963d4734
mh <blueskyareahm@gmail.com>
2018-05-23 22:58:07 +0900

    Try StopWatch.

---
+++ src/main/java/practice/TryStopWatch.java
@@ -0,0 +1,16 @@
+package main.java.practice;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+public class TryStopWatch {
+
+	public static void main(String[] args) throws InterruptedException {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Thread.sleep(5000);
+		stopWatch.stop();
+		
+		System.out.println(stopWatch.getTime());
+	}
+
+}
