df41b780391de7d34ec6b4b13bd8a4059e92c05c
mh <blueskyareahm@gmail.com>
2018-06-07 22:10:20 +0900

    JsonSetter.

---
+++ src/main/java/practice/json/JsonSetterExample.java
@@ -0,0 +1,36 @@
+package main.java.practice.json;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+public class JsonSetterExample {
+	private int key;
+	private String name;
+	private String memo;
+	
+	public int getKey() {
+		return key;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getMemo() {
+		return memo;
+	}
+	
+	@JsonSetter("key")
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	@JsonSetter("name")
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@JsonSetter("memo")
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+}
---
+++ src/test/java/practice/json/JsonSetterExampleTest.java
@@ -0,0 +1,25 @@
+package test.java.practice.json;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.IOException;
+
+import main.java.practice.json.JsonSetterExample;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonSetterExampleTest {
+
+	@Test
+	public void test() throws JsonProcessingException, IOException {
+		String json = "{\"key\":10,\"name\":\"my name\"}";
+		JsonSetterExample entity = new ObjectMapper().readerFor(JsonSetterExample.class).readValue(json);
+		assertThat(entity.getKey(), is(10));
+		assertThat(entity.getName(), is("my name"));
+		assertTrue(entity.getMemo() == null);
+	}
+}
