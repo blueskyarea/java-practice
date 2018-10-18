7d7f4af68ddb9bedb3abf62613518f3b94a96078
mh <blueskyareahm@gmail.com>
2018-10-19 00:20:12 +0900

    anonymous class.

---
+++ src/main/java/practice/anonymous/TryAnonymous.java
@@ -0,0 +1,34 @@
+package main.java.practice.anonymous;
+
+public class TryAnonymous {
+
+	public static void main(String[] args) {
+		new TryAnonymous().execute();
+	}
+	
+	public void execute() {
+		// normal class
+		MySqlDao mySqlDao = new MySqlDao();
+		System.out.println(mySqlDao.getData());
+		
+		// anonymous class
+		Dao mySqlDao2 = new Dao() {
+			@Override
+			public String getData() {
+				return "MySql data from anonymous class";
+			}
+		};
+		System.out.println(mySqlDao2.getData());
+	}
+	
+	interface Dao {
+	    String getData();
+	}
+	
+	class MySqlDao implements Dao {
+		@Override
+		public String getData() {
+			return "MySql data from MySqlDao.class";
+		}
+	}
+}
