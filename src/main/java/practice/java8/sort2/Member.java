f7d1f254c6db615283c7315f3d200a77398f1459
mh <blueskyareahm@gmail.com>
2018-12-26 00:42:23 +0900

    java8 sort primitive int.

---
+++ src/main/java/practice/java8/sort2/Member.java
@@ -0,0 +1,11 @@
+package main.java.practice.java8.sort2;
+
+public class Member {
+	public String name;
+	public int height;
+	
+	public Member(String name, int height) {
+		this.name = name;
+		this.height = height;
+	}
+}
---
+++ src/main/java/practice/java8/sort2/SortMain.java
@@ -0,0 +1,37 @@
+package main.java.practice.java8.sort2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortMain {
+
+	public static void main(String[] args) {
+		new SortMain().start();
+	}
+	
+	private void start() {
+		Member ryu = new Member("ryu", 170);
+		Member ken = new Member("ken", 180);
+		Member guile = new Member("guile", 175);
+		Member chunLi = new Member("chun-Li", 165);
+		
+		List<Member> members = new ArrayList<Member>(){
+			{
+				add(ryu);
+				add(ken);
+				add(guile);
+				add(chunLi);
+			}
+		};
+		members.stream().sorted((member1, member2) -> member2.height - member1.height).forEach(m -> {
+			System.out.println("name: " + m.name + " height: " + m.height);
+		});
+		
+		members.get(0).name = "honda";
+		
+		members.stream().sorted((member1, member2) -> member2.height - member1.height).forEach(m -> {
+			System.out.println("name: " + m.name + " height: " + m.height);
+		});
+	}
+
+}
