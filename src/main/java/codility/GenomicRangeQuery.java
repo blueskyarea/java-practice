aa2a140968e4d0ef0a47909d1f233982b5c46fc0
mh <blueskyareahm@gmail.com>
2019-02-26 01:25:19 +0900

    GenomicRangeQuery.

---
+++ src/main/java/codility/GenomicRangeQuery.java
@@ -0,0 +1,32 @@
+package main.java.codility;
+
+
+public class GenomicRangeQuery {
+
+	public static void main(String[] args) {
+		String S = "CAGCCTA";
+		int[] P = {2, 5, 0};
+		int[] Q = {4, 5, 6};
+		int[] answer = new GenomicRangeQuery().solution(S, P, Q);
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
+	}
+
+	public int[] solution(String S, int[] P, int[] Q) {
+        int[] answer = new int[P.length];    
+        for (int i = 0; i < P.length; i++) {
+        	String tmp = S.substring(P[i], Q[i] + 1);
+        	if (tmp.contains("A")) {
+            	answer[i] = 1;
+            } else if (tmp.contains("C")) {
+            	answer[i] = 2;
+            } else if (tmp.contains("G")) {
+            	answer[i] = 3;
+            } else {
+            	answer[i] = 4;
+            }
+        }
+        return answer;
+    }
+}
